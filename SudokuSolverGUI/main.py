e439b794b9db032bf0a22997cbb7f980ef488600
Raunak Anand <rsanand@ucdavis.edu>
2021-01-03 20:06:40 +0530

    Update main.py

--- SudokuSolverGUI/main.py
+++ SudokuSolverGUI/main.py
@@ -250,7 +250,7 @@ def format_time(secs):
     minute = secs // 60
     hour = minute // 60
 
-    mat = " " + str(hour) + ":" + str(minute) + ":" + str(sec)
+    mat = " " + str(minute) + ":" + str(sec)
     return mat
 
 
